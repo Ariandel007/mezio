991f5c4a1c9643e771240b2d8919bb96b0d5588a
Alexander Gonzalo Urbina Mar <43892122+Ariandel007@users.noreply.github.com>
2020-05-30 18:13:52 -0500

    controllers package

---
+++ Mezio/src/main/java/controllers/CategoriaController.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.models.Categoria;
+import com.example.demo.services.CategoriaService;
+
+@RestController
+@RequestMapping("/categoria")
+public class CategoriaController {
+    CategoriaService categoriaService;
+
+    @Autowired
+    public CategoriaController(CategoriaService categoriaService) {
+        this.categoriaService=categoriaService;
+    }
+    
+    @CrossOrigin
+    @RequestMapping
+    public ResponseEntity<List<Categoria>> listCategorias(){
+    	return ResponseEntity.ok(categoriaService.findCategoriaAll());    
+    }
+    
+
+}
